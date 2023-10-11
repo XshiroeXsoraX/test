0fffe94a108cdbbd1c87d5f2fac07639798029e6
Yeuk Hon John Ng <127374976+XshiroeXsoraX@users.noreply.github.com>
2023-10-11 16:26:19 +1100

    Add files via upload

---
+++ graph_gen.py
@@ -0,0 +1,98 @@
+from plant import Plant
+from plant import Position
+from pymongo import MongoClient
+import pandas as pd
+import matplotlib.pyplot as plt
+import datetime
+import csv
+import json
+import re
+
+from connection import uri
+
+# Connect to MongoDB
+client = MongoClient(uri)
+db = client["SyncUserData"]
+
+# Combine all data into one single dataframe
+def concat_all_data():
+
+    all_data = pd.DataFrame()
+
+    for x in range(1,11):
+
+        mongo_data = db["Decay_"+str(x)].find()
+        mongo_df = pd.DataFrame(mongo_data)
+
+        all_data = pd.concat([all_data, mongo_df])
+
+    all_data.sort_values(by=['Timestamp'])
+    all_data['_id'].astype('string_')
+
+    return all_data
+
+# Generates graphs for overview data
+def overview_data(user):
+
+    
+    mongo_data = db[user].find()
+    mongo_df = pd.DataFrame(mongo_data)
+
+    #Pie chart for plant status
+    fig, ax = plt.subplots()
+    ax.pie(mongo_df['Status'].value_counts(), labels=mongo_df['Status'].unique(), autopct='%1.1f%%')
+    plt.savefig('PyScripts\Graphs\pieStatus_'+str(user)+'.png')
+    plt.close()
+
+    fig, ax = plt.subplots()
+    ax.pie(mongo_df['Plant Type'].value_counts(), labels=mongo_df['Plant Type'].unique(), autopct='%1.1f%%')
+    plt.savefig('PyScripts\Graphs\pieType_'+str(user)+'.png')
+    plt.close()
+
+# Generates graphs for specific plant data
+def specific_data(data):
+
+    grouped = data.groupby(['Position'])        
+
+    #Line graph for moisture
+    for name, group in grouped:
+
+        filename = re.search("\(\d, \d, \d\)", str(name))
+
+        fig, ax = plt.subplots()
+        ax.plot(group['Timestamp'], group['Moisture (%)'])
+        plt.xticks(rotation=90, ha='right')
+        plt.title("Moisture of plant "+str(group['Id'].unique()))
+        plt.xlabel('Timestamp')
+        plt.ylabel('%')
+        plt.savefig('PyScripts\Graphs\moist_'+filename.group()+'.png')
+        plt.close()
+
+    #Line graph for temperature
+    for name, group in grouped:
+
+        filename = re.search("\(\d, \d, \d\)", str(name))
+
+        fig, ax = plt.subplots()
+        ax.plot(group['Timestamp'], group['Temperature (°C)'])
+        plt.xticks(rotation=90, ha='right')
+        plt.title("Temperature of plant "+str(group['Id'].unique()))
+        plt.xlabel('Timestamp')
+        plt.ylabel('C')
+        plt.savefig('PyScripts\Graphs\\temp_'+filename.group()+'.png')
+        plt.close()
+
+    #Line graph for pressure
+    for name, group in grouped:
+
+        filename = re.search("\(\d, \d, \d\)", str(name))
+        
+        fig, ax = plt.subplots()
+        ax.plot(group['Timestamp'], group['Atmospheric Pressure (Pa)'])
+        plt.xticks(rotation=90, ha='right')
+        plt.title("Pressure of plant "+str(group['Id'].unique()))
+        plt.xlabel('Timestamp')
+        plt.ylabel('Pa')
+        plt.savefig('PyScripts\Graphs\pressure_'+filename.group()+'.png')
+        plt.close()
+
---
+++ main.py
@@ -0,0 +1,102 @@
+import graph_gen as gen
+import plant as pl
+import image_transfer as img_t
+import sys
+import fnmatch
+import os
+
+from flask import Flask
+import pymongo
+from bson.json_util import dumps
+from urllib.parse import quote_plus
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import gridfs
+
+
+
+
+
+#Single entry point for all functions related to graph generation and transfer
+
+"""
+Arguements (only one) that are allowed:
+
+all: transfers all graphs
+down: downloads all graphs from database to local storage
+*.png: single png file for transfer
+overview: generates overview graphs
+specific: generates specific plant graphs
+"""
+
+
+
+
+# Original username and password
+username = "admin"
+password = "Password123"
+
+# Escaped username and password
+username_escaped = quote_plus(username)
+password_escaped = quote_plus(password)
+
+uri = f"mongodb+srv://{username_escaped}:{password_escaped}@cluster0.g9kdlqh.mongodb.net/?retryWrites=true&w=majority"
+
+"""
+arguments = sys.argv[1:]
+
+if (len(arguments)!=1):
+    print("Arguments incorrect.")
+    
+else:
+
+    if(arguments[0]=="all"):
+        print("Transferring all")
+        img_t.transfer_all()
+
+    elif(arguments[0]=="down"):
+        img_t.download_all()
+    
+    elif(fnmatch.fnmatch(arguments[0], '*.png')):
+
+        graph_dir_path = 'PyScripts\\Graphs'
+        graph_list = []
+
+        for file_path in os.listdir(graph_dir_path):
+            # check if current file_path is a file
+            if os.path.isfile(os.path.join(graph_dir_path, file_path)):
+                # add filename to list
+                graph_list.append(file_path)
+        if(arguments[0] not in graph_list):
+            print("File is not in folder.")
+        else:
+            print("Transferring "+arguments[0])
+            img_t.transfer_one(arguments[0])
+
+    elif(arguments[0]=='overview'):
+        print("Generating overview graphs.")
+        gen.overview_data()
+    elif(arguments[0]=='specific'):
+        print("Generating specific graphs.")
+        all_data = gen.concat_all_data()
+        gen.specific_data(all_data)
+    else:
+        print("Arguments incorrect.")
+
+"""
+
+client = MongoClient(uri)
+db = client["SyncUserData"]
+gdb = client['Graphs']
+fs = gridfs.GridFS(gdb, collection="data_graphs")
+
+with client["SyncUserData"].watch() as stream:
+
+    print("Listening to changes")
+    for change in stream:
+        print(dumps(change['ns']['coll']))
+        print("Generating overview graphs for "+change['ns']['coll']+".")
+        user = change['ns']['coll']
+        gen.overview_data(user)
+        print("Transferring all")
+        img_t.transfer_all()
